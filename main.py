559e17bdce5daac31e72460ae2aa59b4a01193d2
jared <jaredpilbeam@yahoo.com>
2026-04-21 16:12:31 -0400

    Added data input validation for the scheduling using cell widgets.
    Removed the interval class. Intervals will be tracked using a list.
    
    Shifting around code to prep for scheduling functionality.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import datetime
 import subprocess
 import time
+import threading
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 from PyQt6.QtCore import *
@@ -28,7 +29,7 @@ class PyNetworkTimer(QWidget):
         self.setWindowTitle("PyNetwork Timer")
         self.setWindowIcon(QIcon("placedholder_icon.jpg"))
 
-        self.setGeometry(500, 500, 652, 400)
+        self.setGeometry(500, 500, 452, 400)
         self.initUI()
 
     def initUI(self):
@@ -48,6 +49,7 @@ class PyNetworkTimer(QWidget):
             j=0
             for line in get_linux_devs().splitlines():
                 split_line = line.decode().split(":")
+                
                 device = NetworkDevice(split_line[0],split_line[1],split_line[2],
                                    split_line[3])
                 device_list.append(device)
@@ -58,10 +60,18 @@ class PyNetworkTimer(QWidget):
                 if( i % 4 == 3):
                      j += 1
                      i = 1
+                if(len(get_linux_devs().splitlines()) == 1):
+                     status_page_layout.addWidget(QLabel(""), j, i, 1, 2)
+                elif(len(get_linux_devs().splitlines()) == 2):
+                    status_page_layout.addWidget(QLabel(""), j, i)
             status_page_layout.addWidget(QLabel(""))
             status_page_layout.setRowStretch((j+1),1)
             status_page_layout.addWidget(QLabel(QTime.currentTime().toString("hh:mm:ss")),j+2,0,1,2)
-            status_page_layout.addWidget(QPushButton("Start Schedule"),j+2,2)
+            schedule_on = QPushButton("Start Schedule")
+            schedule_off = QPushButton("Stop Schedule")
+            status_page_layout.addWidget(schedule_on,j+2,1)
+            status_page_layout.addWidget(schedule_off,j+2,2)
+            
 
         elif os.name == "nt":
             get_win_devs()
@@ -72,23 +82,20 @@ class PyNetworkTimer(QWidget):
         schedule_page.setLayout(schedule_page_layout)
         table = QTableWidget(schedule_page)
         table.setRowCount(len(interval_testing))
-        table.setColumnCount(6)
-        table.setHorizontalHeaderLabels(["Start Time", "End time","Day","Month",
-                                         "Year","Day of Week"])
+        table.setColumnCount(4)
+        table.setHorizontalHeaderLabels(["Start Time", "End time","Date","Day of Week"])
         schedule_page_layout.addWidget(table)
         add_button = QPushButton("Add Interval")
         remove_button = QPushButton("Remove Interval")
-        start_test_button = QPushButton("TEST transfer to list of lists")
         add_button.clicked.connect(lambda: self.add_button_click(table, interval_testing))
         remove_button.clicked.connect(lambda: self.remove_button_click(table, interval_testing))
-        start_test_button.clicked.connect(lambda: self.start_test_click(table, interval_testing))
         schedule_page_layout.addWidget(remove_button)
         schedule_page_layout.addWidget(add_button)
-        schedule_page_layout.addWidget(start_test_button)
-       
+        schedule_on.clicked.connect(lambda: self.schedule_on_click(table, interval_testing))
         tab.addTab(status_page, "Status")
         tab.addTab(schedule_page, "Schedule")
         self.main_layout.addWidget(tab, 0, 0, 2, 1)
+        
 
     def remove_button_click(self,table, interval_testing):
         if(len(interval_testing) > 0 and table.currentRow() != -1):
@@ -104,29 +111,42 @@ class PyNetworkTimer(QWidget):
         else: 
              pass
             
-    def add_button_click(self, table, interval_testing):
+    def add_button_click(self, table, intervals):
          table.insertRow(table.rowCount())
-         for i in range(0,6):
-              table.setItem((table.rowCount()-1), i, QTableWidgetItem(" "))
-         return interval_testing.append(["","","","","",""])
+         interval_dropdown = QComboBox()
+         interval_dropdown.addItems(['Monday','Tuesday','Wednesday','Thursday',
+                                    'Friday', 'Saturday', 'Sunday'])
+         interval_date = QDateEdit(self)
+         interval_date.editingFinished.connect(interval_date.update)
+         for i in range(0,4):
+            table.setItem((table.rowCount()-1), i, QTableWidgetItem())
+            if i == 0:
+                table.setCellWidget((table.rowCount()-1), i, QTimeEdit(self))
+            elif i == 1:
+                table.setCellWidget((table.rowCount()-1), i, QTimeEdit(self))
+            elif i == 2:
+                 table.setCellWidget((table.rowCount()-1), i, interval_date)
+            elif i == 3:
+                 table.setCellWidget((table.rowCount()-1), i, interval_dropdown)
+         return intervals.append(["","","",""])
     
-    def start_test_click(self, table, interval_testing):
+    def schedule_on_click(self, table, intervals):
         for i in range(0, table.rowCount()):
-            for j in range(0, 6):
+            for j in range(4):
                 if(table.item(i,j) is None) :
-                     interval_testing[i][j] = ""
-                else:interval_testing[i][j] = table.item(i,j).text()
+                     intervals[i][j] = ""
+                elif j in range(3):
+                     intervals[i][j] = table.cellWidget(i,j).text()
+                elif j == 3:
+                     intervals[i][j] = table.cellWidget(i,j).currentText()
+        print(intervals)
+        self.schedule_run_loop()
 
-    # stores disconnection interval
-class Interval:
-    def __init__(self, start_time, end_time, day_of_month="", month="", year="",
-                day_of_week=""):
-        self.day_of_month = day_of_month
-        self.month = month
-        self.year = year
-        self.day_of_week = day_of_week
-        self.start_time = start_time
-        self.end_time = end_time
+    def schedule_run_loop(self):
+         pass
+
+    def schedule_off_click(self):
+         pass
 
     # stores network devices, their states and connections.
 class NetworkDevice:
@@ -154,17 +174,6 @@ class NetworkDevice:
     def __str__(self):
         return f"""Name: {self.name} State: {self.state} Type: {self.type} 
         Connection: {self.connection}"""
-    
-interval = Interval("14:01","19:59","0","0","0","0")
-now = datetime.datetime.now()
-compare_start_time = now.replace(hour=
-                                int(interval.start_time.split(":")[0]),
-                                minute=
-                                int(interval.start_time.split(":")[1]))
-compare_end_time = now.replace(hour=
-                                int(interval.end_time.split(":")[0]),
-                                minute=
-                                int(interval.end_time.split(":")[1]))
                
 if __name__=="__main__":
     app = QApplication(sys.argv)
